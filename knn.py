00004d16dd84937072252a33b301b8730d8281c5
ssvas1997 <37727994+ssvas1997@users.noreply.github.com>
2019-03-09 12:06:12 +0530

    Add files via upload

--- knn.py
+++ knn.py
@@ -1,42 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  9 09:32:38 2019
-
-@author: mca
-"""
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
-df= pd.read_csv("E:\Srinivas\KNN\wine.csv")
-features= df.drop('class',axis=1).values
-classes=df['class'].values
-(train_feat,test_feat,train_classes,test_classes)= train_test_split(features,classes,train_size=0.8,random_state=10)
+df = pd.read_csv("E:\Srinivas\KNN\wine.csv")
+df.dtypes
 
-knn= KNeighborsClassifier(n_neighbors=4)
-knn.fit(train_feat,train_classes)
-pred=knn.predict(test_feat)
-print("Accuracy:",metrics.accuracy_score(test_classes,pred))
 
-dft= pd.read_csv("E:\Srinivas\KNN\wine2.csv")
-feat=dft[dft.columns].values
-pred=knn.predict(feat)
-print("Target Class",pred)
+features = df.drop('class',axis=1).values
 
-neighbors= np.arange(1,9) #k value
-train_accuracy= np.empty(len(neighbors))
-test_accuracy= np.empty(len(neighbors))
+classes = df['class'].values
+
+(train_feat,test_feat,train_class,test_class) = train_test_split(features,classes,train_size = 0.8,random_state=40)
+
+knn = KNeighborsClassifier(n_neighbors=25)
+knn.fit(train_feat,train_class)
+pred = knn.predict(test_feat)
+
+
+print("accuracy",metrics.accuracy_score(test_class,pred))
+#df.describe()
+dft = pd.read_csv("E:\Srinivas\KNN\wine2.csv")
+feat = dft[dft.columns].values
+pred = knn.predict(feat)
+print("Target Class :",pred)
+
+neighbors = np.arange(1,9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
 for i,k in enumerate(neighbors):
-    #Set up a knn classifier with k neighbors
-    knn= KNeighborsClassifier(n_neighbors=k)
-    #Fit the model
-    knn.fit(train_feat,test_classes)
-    #Compute Accuracy on the training set
-    train_accuracy[i] = knn.score(train_feat,train_classes)
-    #Compute accuracy on the test set
-    test_accuracy[i]= knn.score(test_feat,test_classes)
     
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(train_feat,train_class)
+    train_accuracy[i]= knn.score(train_feat,train_class)
+    test_accuracy[i] = knn.score(test_feat,test_class)
+    print("test",test_accuracy[i])
+    print("train",train_accuracy[i])
+    
+plt.title("KNN Varying number of neighbors ")    
+plt.plot(neighbors,test_accuracy,label='Testing Accuracy')
+plt.plot(neighbors,train_accuracy,label = 'Training Accuracy')
+plt.legend()
+plt.xlabel("Number of Neighbor")
+plt.ylabel('Accuracy')
+plt.show()
